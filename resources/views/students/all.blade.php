94762c85447bd8143ec4d50cf99fca6e79f53552
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-11-17 16:12:47 -0500

    ui

--- resources/views/students/all.blade.php
+++ resources/views/students/all.blade.php
@@ -2,28 +2,63 @@
 
 @section('content')
 
-@foreach ($students as $student)
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-xl-12 mb-5">
+            <div class="card">
+                <div class="card-header bg-white text-center mb-3 pt-4">
+                    <h3 class="card-title">
+                        Todos los registros de estudiantes
+                    </h3>
+                </div>
+                <div class="card-body">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            @if (count($students) !== 0)
 
-<a class="btn btn-info" href="{{route('students.showStudent', $student->id)}}">Ver</a>
+                                @foreach ($students as $student)
 
-<a class="btn btn-dark" href="{{route('people.editStudent', $student->id)}}">editar</a>
+                                <div class="col-md-2 d-flex align-items-center">
+                                    <img src="{{Gravatar::get($student->email)}}" alt="peopleGravatar">
+                                </div>
 
-<form action="{{route('people.deleteStudent', $student->id)}}" method="POST" class="d-inline">
-    @method('DELETE')
-    @csrf
-    <button class="btn btn-danger" type="submit">
-        Eliminar
-    </button>
-</form>
+                                <div class="col-md-8 d-flex align-items-center justify-content-between">
+                                    <span>{{$student->fullname}}</span>
 
-{{$student->username}}
+                                    <span>{{$student->email}}</span>
 
-{{$student->email}}
+                                    <span>{{$student->type}}</span>
 
-{{$student->role}}
+                                    <span>{{$student->code}}</span>
 
-<img src="{{Gravatar::get($student->email)}}" alt="userGravatar">
+                                </div>
 
-@endforeach
+                                <div class="col-md-2 d-flex align-items-center">
+                                    <a class="btn btn-dark mr-2" href="{{route('people.editStudent', $student->id)}}">editar</a>
+                                <form action="{{route('people.deleteStudent', $student->id)}}" method="POST" class="d-inline">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button class="btn btn-danger" type="submit">
+                                        Eliminar
+                                    </button>
+                                </form>
+                                </div>
+
+                                @endforeach
+
+                                @else
+
+                                <div class="col-md-12">
+                                    <p>No hay registros de Estudiantes</p>
+                                </div>
+
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
