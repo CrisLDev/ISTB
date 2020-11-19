3dd35edef99ac01c0e595aadecb59aca3c3dfc54
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-11-19 00:21:55 -0500

    Lista la primer version de la app 1.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -107,12 +107,14 @@
                 <div class="container d-flex justify-content-between">
                     @guest
                     @else
+                    @can('isCoor')
                     <button type="button" id="sidebarCollapse" class="btn btn-light">
                         <i class="fas fa-align-left"></i>
                         <span><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                           </svg></span>
                     </button>
+                    @endCan
                     @endguest
                     <a class="navbar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
                         <img class="w-25 mr-2" src="{{ asset('assets/logo.svg') }}" alt="logo">
