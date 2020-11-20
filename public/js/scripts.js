da7cf6a51216807ca54b022944a2b34538bbe74f
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-11-19 20:04:21 -0500

    Algo q no jalaba

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,6 @@
 function disable() {
     document.getElementById("button-prevent-multiple-submits").disabled = true;
     document.getElementById("button-prevent-multiple-submits2").disabled = true;
-    document.getElementById("spinner").style.display='inline-block';
-    document.getElementById("btex").innerText='Cargando';
+    //document.getElementById("spinner").style.display='inline-block';
+    //document.getElementById("btex").innerText='Cargando';
 }
