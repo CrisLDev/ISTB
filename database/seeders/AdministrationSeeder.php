a09318d1b805c3e16cc043fe6ff476df3f951a1d
CrisLDvev <leonardomoreirapazmio@gmail.com>
2020-11-20 14:55:01 -0500

    Listo el seeder de administracion

--- database/seeders/AdministrationSeeder.php
+++ database/seeders/AdministrationSeeder.php
@@ -16,11 +16,11 @@ class AdministrationSeeder extends Seeder
     public function run()
     {
         Administration::create([
-            'id' => 1,
+            'id' => '1',
             'fullname' => 'Alfredo Manuel Bautista Encina',
-            'user_id' => 1,
-            'telephoneNumber' => 0985621452,
-            'dni' => 1203652145,
+            'user_id' => '1',
+            'telephoneNumber' => '0985621452',
+            'dni' => '1203652145',
             'address' => 'km 20 via a la costa',
             'age' => '20',
             'email' => 'leonardomoreirapazmio@gmail.com',
@@ -29,11 +29,11 @@ class AdministrationSeeder extends Seeder
             'role' => 'Director'
         ]);
         Administration::create([
-            'id' => 2,
+            'id' => '2',
             'fullname' => 'Alejandro Bautista Mejía',
-            'user_id' => 1,
-            'telephoneNumber' => 0145232789,
-            'dni' => 1103652145,
+            'user_id' => '1',
+            'telephoneNumber' => '0145232789',
+            'dni' => '1103652145',
             'address' => 'Av de las americas',
             'age' => '23',
             'email' => 'bausmeji@gmail.com',
@@ -42,11 +42,11 @@ class AdministrationSeeder extends Seeder
             'role' => 'Director de talento humano'
         ]);
         Administration::create([
-            'id' => 3,
+            'id' => '3',
             'fullname' => 'Juan Miguel de Jesús Bautista Vázquez',
-            'user_id' => 1,
-            'telephoneNumber' => 0932652326,
-            'dni' => 1503652145,
+            'user_id' => '1',
+            'telephoneNumber' => '0932652326',
+            'dni' => '1503652145',
             'address' => 'Avenida Francisco de Orellana',
             'age' => '50',
             'email' => 'basuvasq@gmail.com',
@@ -55,11 +55,11 @@ class AdministrationSeeder extends Seeder
             'role' => 'Secretario'
         ]);
         Administration::create([
-            'id' => 4,
+            'id' => '4',
             'fullname' => 'Gregorio Benítez Ferrusquía',
-            'user_id' => 1,
-            'telephoneNumber' => 0925262535,
-            'dni' => 1103652145,
+            'user_id' => '1',
+            'telephoneNumber' => '0925262535',
+            'dni' => '1103652145',
             'address' => 'Avenida Malecón Simón Bolívar',
             'age' => '70',
             'email' => 'beisferru@gmail.com',
@@ -68,11 +68,11 @@ class AdministrationSeeder extends Seeder
             'role' => 'Transportista'
         ]);
         Administration::create([
-            'id' => 5,
+            'id' => '5',
             'fullname' => 'José Bermúdez Manrique',
-            'user_id' => 1,
-            'telephoneNumber' => 0965986593,
-            'dni' => 1103619145,
+            'user_id' => '1',
+            'telephoneNumber' => '0965986593',
+            'dni' => '1103619145',
             'address' => 'Avenida Malecón Simón Bolívar',
             'age' => '30',
             'email' => 'jbermud@gmail.com',
@@ -81,17 +81,69 @@ class AdministrationSeeder extends Seeder
             'role' => 'Transportista'
         ]);
         Administration::create([
-            'id' => 6,
-            'fullname' => 'José Bermúdez Manrique',
-            'user_id' => 1,
-            'telephoneNumber' => 0965986593,
-            'dni' => 1103619145,
-            'address' => 'Avenida Malecón Simón Bolívar',
+            'id' => '6',
+            'fullname' => 'Salvador de la Cruz Hernandez',
+            'user_id' => '1',
+            'telephoneNumber' => '0945986593',
+            'dni' => '1000619145',
+            'address' => 'Calle florez',
             'age' => '30',
-            'email' => 'jbermud@gmail.com',
+            'email' => 'sdlchernnandez@gmail.com',
             'code' => rand(),
             'birthDate' => '1990-11-20',
-            'role' => 'Transportista'
+            'role' => 'Encargado de bienestar estudiantil'
+        ]);
+        Administration::create([
+            'id' => '7',
+            'fullname' => 'Gigliola Valencia Murillo',
+            'user_id' => '1',
+            'telephoneNumber' => '0943186593',
+            'dni' => '1111619145',
+            'address' => 'Calle Olmedo',
+            'age' => '30',
+            'email' => 'gigliolamurillo123@gmail.com',
+            'code' => rand(),
+            'birthDate' => '1990-11-20',
+            'role' => 'Encargada de bienestar estudiantil'
+        ]);
+        Administration::create([
+            'id' => '8',
+            'fullname' => 'Miguel Valencia Murillo',
+            'user_id' => '1',
+            'telephoneNumber' => '0943125593',
+            'dni' => 1111259145,
+            'address' => 'Calle Olmedo',
+            'age' => '40',
+            'email' => 'miguelamurillo123@gmail.com',
+            'code' => rand(),
+            'birthDate' => '1980-12-20',
+            'role' => 'Encargado de bienestar estudiantil'
+        ]);
+        Administration::create([
+            'id' => '9',
+            'fullname' => 'Anacleto Valencia Murillo',
+            'user_id' => '1',
+            'telephoneNumber' => '0943186593',
+            'dni' => '1111619145',
+            'address' => 'Calle Olmedo',
+            'age' => '50',
+            'email' => 'aaaaaaaanacleto@gmail.com',
+            'code' => rand(),
+            'birthDate' => '1970-05-20',
+            'role' => 'Segundo encargado de bienestar estudiantil'
+        ]);
+        Administration::create([
+            'id' => '10',
+            'fullname' => 'Andres Coello Goyes',
+            'user_id' => '1',
+            'telephoneNumber' => '0965129865',
+            'dni' => '1391619145',
+            'address' => 'Calle Junin frente av olmedo',
+            'age' => '60',
+            'email' => 'aaaaaaaanacleto@gmail.com',
+            'code' => rand(),
+            'birthDate' => '1960-10-12',
+            'role' => 'Tercer encargado de bienestar estudiantil'
         ]);
     }
 }
